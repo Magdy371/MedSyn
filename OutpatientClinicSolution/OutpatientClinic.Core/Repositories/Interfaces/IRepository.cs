a6e0d9973c9e8906d3f7c532722edf1bc434558d
Magdy Elshrief <73464211+Magdy371@users.noreply.github.com>
2025-03-07 17:38:56 +0200

    Update IRepository.cs Generic Interface Created

--- OutpatientClinicSolution/OutpatientClinic.Core/Repositories/Interfaces/IRepository.cs
+++ OutpatientClinicSolution/OutpatientClinic.Core/Repositories/Interfaces/IRepository.cs
@@ -1,25 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace OutpatientClinic.DataAccess.Repositories.Interfaces
+namespace OutpatientClinic.Core.Repositories.Interfaces
 {
     public interface IRepository<T>
     {
         //Basic CRUD Operations
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
-        Task<T> AddSync(T entity);
-        Task<T> UpdateSync(T entity);
-        Task<T> DeleteSync(T entity);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
 
-        //Query and Helper methods
+        // Query and helper methods
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
         Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
         Task<int> CountAsync();
         IQueryable<T> Query();
-
     }
 }

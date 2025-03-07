66e5962f08dd0bd82945ecd38ee045bb2c0ba4af
Magdy Elshrief <magdyelshrief37@gmail.com>
2025-03-07 17:23:33 +0200

    Repository For MedSync Created (Generic Interface / Implementation)

--- OutpatientClinicSolution/OutpatientClinic.Core/Repositories/Implementations/Repository.cs
+++ OutpatientClinicSolution/OutpatientClinic.Core/Repositories/Implementations/Repository.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using OutpatientClinic.Core.Repositories.Interfaces;
+using System.Linq.Expressions;
 
-namespace OutpatientClinic.DataAccess.Repositories.Implementations
+namespace OutpatientClinic.Core.Repositories.Implementations
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() =>
+            await _dbSet.AsNoTracking().ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) =>
+            await _dbSet.FindAsync(id) ?? throw new InvalidOperationException($"Entity with id {id} not found.");
+
+
+        public async Task AddAsync(T entity) =>
+            await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) =>
+            _dbSet.Update(entity);
+
+        public void Delete(T entity) =>
+            _dbSet.Remove(entity);
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) =>
+            await _dbSet.AsNoTracking().Where(predicate).ToListAsync();
+
+        public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate) =>
+            await _dbSet.AnyAsync(predicate);
+
+        public async Task<int> CountAsync() =>
+            await _dbSet.CountAsync();
+
+        public IQueryable<T> Query() =>
+            _dbSet.AsQueryable();
     }
 }

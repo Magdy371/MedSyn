51a465dd328fffcb85648eaf69c725a90a5c368a
Magdy Elshrief <magdyelshrief37@gmail.com>
2025-03-07 16:47:16 +0200

    Repository Geberic Interface Created "IRepository"

--- OutpatientClinicSolution/OutpatientClinic.Core/Repositories/Interfaces/IRepository.cs
+++ OutpatientClinicSolution/OutpatientClinic.Core/Repositories/Interfaces/IRepository.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace OutpatientClinic.DataAccess.Repositories.Interfaces
 {
-    internal class IRepository
+    public interface IRepository<T>
     {
+        //Basic CRUD Operations
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddSync(T entity);
+        Task<T> UpdateSync(T entity);
+        Task<T> DeleteSync(T entity);
+
+        //Query and Helper methods
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+        Task<int> CountAsync();
+        IQueryable<T> Query();
+
     }
 }
